a6b300ff2fc113ed9973e6477f8240321472f910
Litery <s.litera@gmail.com>
2018-04-07 21:28:35 +0200

    PEP8

--- tests/test_simulation/test_polygon.py
+++ tests/test_simulation/test_polygon.py
@@ -4,20 +4,22 @@ from unittest import TestCase
 
 from simulation.simulation import Polygon
 
+
 def construct_polygon(poly):
     x, y = zip(*poly)
     return Polygon(x, y)
+
 
 class TestPolygon(TestCase):
     def test_not_collides_1(self):
         points1 = [(1.0, 2.0), (1.0, 1.0), (0.0, 1.0), (1.0, 2.0)]
         points2 = [(-1.0, -1.0),
-                (-1.0, 1.0),
-                (0.0, 0.5),
-                (1.0, 1.0),
-                (1.0, -1.0),
-                (0.0, -0.5),
-                (-1.0, -1.0)]
+                   (-1.0, 1.0),
+                   (0.0, 0.5),
+                   (1.0, 1.0),
+                   (1.0, -1.0),
+                   (0.0, -0.5),
+                   (-1.0, -1.0)]
         poly1 = construct_polygon(points1)
         poly2 = construct_polygon(points2)
         assert not poly1.collides(poly2)
@@ -25,12 +27,12 @@ class TestPolygon(TestCase):
     def test_collides_1(self):
         points1 = [(0.0, 1.0), (0.0, 0.0), (-1.0, 0.0), (0.0, 1.0)]
         points2 = [(-1.0, -1.0),
-                (-1.0, 1.0),
-                (0.0, 0.5),
-                (1.0, 1.0),
-                (1.0, -1.0),
-                (0.0, -0.5),
-                (-1.0, -1.0)]
+                   (-1.0, 1.0),
+                   (0.0, 0.5),
+                   (1.0, 1.0),
+                   (1.0, -1.0),
+                   (0.0, -0.5),
+                   (-1.0, -1.0)]
         poly1 = construct_polygon(points1)
         poly2 = construct_polygon(points2)
         assert poly1.collides(poly2)
